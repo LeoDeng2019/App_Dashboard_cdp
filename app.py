c2f408aadf18b0f4317de3ae628943d78fd6d890
Leo <67538649+LeoDeng2019@users.noreply.github.com>
2023-04-19 17:36:13 +0800

    jb

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import streamlit as st
 
+st.set_page_config(
+    page_title='蔡大胖数据分析看板'
+)
+
 ### 获取数据
 df_raw = pd.read_csv('df_irv_table.csv')
-
 st.dataframe(df_raw.head())
+
+### sidebar
